3eb9ba8d126cb6edb5b2bfebf93b81fdc68a4daa
Tomas Aguirre <tomasignaguirre@gmail.com>
2023-08-26 13:07:02 -0300

    Delete ParadigmasDeProgramacion directory

--- ParadigmasDeProgramacion/1-2.py
+++
@@ -1,6 +0,0 @@
-###2 - Cuadrado de un binomio Plantear un script directamente en el shell de Python, que permita mostrar, para dos valores 𝑎 y 𝑏, que: Un binomio al cuadrado (suma) esigual al cuadrado del primer término, más el doble producto del primero por el segundo más el cuadrado del segundo. ###
-valorA=float(input("Ingrese primer valor: "));
-valorB=float(input("Ingrese segundo valor: "));
-binomio=(valorA+valorB)**2;
-formula=valorA**2+2*valorA*valorB+valorB**2;
-print(f'La resolucion del binomio da como resultado {binomio} y La resolucion de la formula da como resultado {formula}');
--- ParadigmasDeProgramacion/1-3.py
+++
@@ -1,5 +0,0 @@
-###3 - Área de un triángulo Desarrolle un programa para calcular el área de un triángulo, cargando por teclado el valor de la base, pero sabiendo que su altura es igual al cuadrado de la base. ###
-base=float(input("Ingrese la medida 'base' del triangulo: "));
-altura=base**2;
-area=(base*altura)/2;
-print(f'El area del triangulo es {area}');
